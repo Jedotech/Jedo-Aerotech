c6d200b7e5b08472a75f265c230c3383ed974a13
Jedotech <tajesudoss@gmail.com>
2026-03-05 16:39:43 +0530

    Fixed schema files with exact spacing

---
+++ src/sanity/schemaTypes/part.ts
@@ -0,0 +1,23 @@
+export const part = { (0 spaces)
+
+  name: 'part',
+
+  type: 'document',
+
+  title: 'Aircraft Part',
+
+  fields: [
+
+    {
+
+      name: 'partNumber',
+
+      type: 'string',
+
+      title: 'Part Number',
+
+    },
+
+  ],
+
+}
