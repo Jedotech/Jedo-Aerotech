8ddb590494df0f1d161bc9927e49945d3156effe
Jedotech <tajesudoss@gmail.com>
2026-03-14 18:59:54 +0530

    Fix: Updated Sanity config with basePath and project ID

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { schema } from './src/sanity/schemaTypes'
+// Ensure this path matches where your schema/index.ts is located
+import { schema } from './src/sanity/schemaTypes' 
 
 export default defineConfig({
-name: 'default',
-title: 'Jedo Admin',
-projectId: 'm2pa474h',
-dataset: 'production',
-basePath: '/studio',
-plugins: [
-structureTool(),
-],
-schema: {
-types: schema.types,
-},
+  name: 'default',
+  title: 'Jedo Admin',
+
+  projectId: 'm2pa474h',
+  dataset: 'production',
+  
+  // This allows you to access the studio at jedotech.com/studio
+  basePath: '/studio', 
+
+  plugins: [
+    structureTool(),
+  ],
+
+  schema: {
+    // If your schema object has a 'types' property, this is correct
+    types: schema.types, 
+  },
 })
