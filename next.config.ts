bfc9c72072839524907705b5060b60a8dad21949
Jedotech <tajesudoss@gmail.com>
2026-04-03 17:28:36 +0530

    Build: Finalized Vercel configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // DELETE or COMMENT OUT this line:
-  // output: 'export', 
-
   images: {
     unoptimized: true, 
   },
